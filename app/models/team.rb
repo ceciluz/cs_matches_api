53239d43872fa8ee2231804e85423a1164a67ace
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-02-27 09:33:45 -0300

    model altarations: match and team

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,7 @@ class Team < ApplicationRecord
   validates :country, presence: true
   validates :region, presence: true
 
-  has_many :players
-  has_many :matches_as_home
-  has_many :matches_as_away
+  has_many :players, dependent: :destroy
+  has_many :matches_as_home_team, class_name: 'Match', foreign_key: 'home_team_id', dependent: :destroy
+  has_many :matches_as_away_team, class_name: 'Match', foreign_key: 'away_team_id', dependent: :destroy
 end
