700e4d38a52db1225d735420c15d2ee66be64974
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-02-27 12:09:31 -0300

    feat: creates Matches Controller

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,15 @@
+class MatchesController < ApplicationController
+  def create
+    @match = Match.new(match_params)
+    if @match.save
+      render json: @match, status: :created
+    else
+      render json: @match.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def match_params
+    params.require(:match).permit(:team_home_id, :team_away_id, player_performances_attributes: [:player_id, :kills, :assists, :deaths, :headshots])
+  end
+end
