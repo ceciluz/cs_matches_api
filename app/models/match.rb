4bbd47547665a94a2bb79f979685ee752e720150
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-02-28 15:54:46 -0300

    adds: functions match_name and win? to match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,4 +8,12 @@ class Match < ApplicationRecord
   belongs_to :team_home, class_name: 'Team', inverse_of: :matches_as_home_team
   belongs_to :team_away, class_name: 'Team', inverse_of: :matches_as_away_team
   accepts_nested_attributes_for :player_performances
+
+  def match_name
+    "#{team_home.name} x #{team_away.name}"
+  end
+
+  def win?
+    team_home_score > team_away_score
+  end
 end
