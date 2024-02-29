fd2c38f64ee96287b8a54d24c7df0430aa1b8e17
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-02-29 08:59:21 -0300

    merge endpoint-matches and endpoint-players

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -1,25 +1,3 @@
-require 'rails_helper'
-
-# RSpec.describe "Matches", type: :request do
-#   describe 'POST #create' do
-#     context 'with valid parameters' do
-#       let(:valid_params) { FactoryBot.attributes_for(:match) }
-
-#       it 'creates a new match' do
-#         expect {
-#           post matches_path(params: { match: valid_params })
-#         }.to change(Match, :count).by(1)
-#       end
-
-#       it 'returns a successful response' do
-#         post matches_path(params:{ match: valid_params })
-#         expect(response).to have_http_status(:created)
-#       end
-#     end
-#   end
-# end
-
-# spec/requests/matches_spec.rb
 
 require 'swagger_helper'
 
