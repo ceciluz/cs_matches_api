e22ad8bec728c81f8c09347e16ddf92040ec88ec
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-03-01 15:48:01 -0300

    refactor:player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+MINIMUM_AGE = 18
 class Player < ApplicationRecord
   belongs_to :team
   validates :name, :nickname, :nationality, :birth_date, presence: true
@@ -8,7 +8,7 @@ class Player < ApplicationRecord
   has_many :performances, class_name: 'PlayerPerformance', dependent: :destroy
 
   def minimum_age
-    return unless birth_date.present? && birth_date > 18.years.ago.to_date
+    return unless birth_date.present? && birth_date > MINIMUM_AGE.years.ago.to_date
 
     errors.add(:birth_date, 'must be at least 18 years old to participate in games')
   end
