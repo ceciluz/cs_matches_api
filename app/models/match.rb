de46e653a2278e486b213d225425bf52433760b8
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-03-01 16:55:06 -0300

    refactor: correct few parameters on model match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Match < ApplicationRecord
-  belongs_to :home_team, class_name: 'Team', inverse_of: :matches_as_home_team
-  belongs_to :away_team, class_name: 'Team', inverse_of: :matches_as_away_team
+  belongs_to :team_home, class_name: 'Team', inverse_of: :matches_as_home_team
+  belongs_to :team_away, class_name: 'Team', inverse_of: :matches_as_away_team
 
   validates :team_home_score, :team_away_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   has_many :player_performances, class_name: 'PlayerPerformance', dependent: :destroy
--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -13,17 +13,20 @@ RSpec.describe 'Counter Strike API', type: :request do
       parameter name: :match, in: :body, schema: { '$ref' => '#/components/schemas/match' }
 
       response 201, 'Created' do
-        let(:team_home) { create(:team, id: 1) }
-        let(:team_away) { create(:team, id: 2) }
-        let(:player) { create(:player, id: 1) }
+        let(:team_home) { create(:team) }
+        let(:team_away) { create(:team) }
+        let(:player) { create(:player) }
         let(:player_performances) { create_list(:player_performance, 5, player:) }
-        let(:match) { build(:match, team_home:, team_away:, player_performances:) }
-        run_test!
+        response 201, 'Created' do
+          let(:match) { build(:match, team_home:, team_away:, player_performances:) }
+          run_test!
+        end
       end
+
       response '422', 'Unprocessable Entity' do
-        let(:team_home) { create(:team, id: 1) }
-        let(:team_away) { create(:team, id: 2) }
-        let(:player) { create(:player, id: 1) }
+        let(:team_home) { create(:team) }
+        let(:team_away) { create(:team) }
+        let(:player) { create(:player) }
         let(:player_performances) { create_list(:player_performance, 5, player:) }
         let(:invalid_match) do
           { team_home_id: nil, team_away_id: nil, team_home_score: nil, team_away_score: nil,
