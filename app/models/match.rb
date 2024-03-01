95f0bc5398d4c549974d350d0f7223df0fcc2953
Cecilia Almeida <cecilia.almeida@codeminer42.com>
2024-03-01 16:15:31 -0300

    merge branch models

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,15 +5,8 @@ class Match < ApplicationRecord
   belongs_to :away_team, class_name: 'Team', inverse_of: :matches_as_away_team
 
   validates :team_home_score, :team_away_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  has_many :players_performances, class_name: 'PlayerPerformance', dependent: :destroy
-
-  validates :team_home_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :team_away_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
   has_many :player_performances, class_name: 'PlayerPerformance', dependent: :destroy
-  belongs_to :team_home, class_name: 'Team', inverse_of: :matches_as_home_team
-  belongs_to :team_away, class_name: 'Team', inverse_of: :matches_as_away_team
+
   accepts_nested_attributes_for :player_performances
 
   def match_name
